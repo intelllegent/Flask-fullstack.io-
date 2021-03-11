c95e8f8885cdc30a89cc9a6c511ffaef586d53c2
intelegent <doom_297@mail.ru>
2021-03-11 18:52:41 +0300

    App from lesson 4
    Signed-off-by: intelegent <doom_297@mail.ru>

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+API_URL = 'https://financialmodelingprep.com//api/v3/stock/real-time-price/{ticker}'
+
+
+def fetch_price(ticker):
+    data = requests.get(API_URL.format(ticker=ticker.upper()),
+                        params={'apikey': 'demo'}).json()
+    return data["price"]
+
+
+@app.route('/stock/<ticker>')
+def stock(ticker):
+    price = fetch_price(ticker)
+    return "The price of {ticker} is {price}".format(ticker=ticker,
+                                                     price=price)
+
+
+@app.route('/')
+def home_page():
+    return "Try /stock/AAPL"
+
+
+if __name__ == '__main__':
+    app.run()
